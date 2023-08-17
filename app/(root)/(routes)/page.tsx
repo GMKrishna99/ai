d09605da9d1508b69bd19eaedc8f72208401e9b7
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-08-17 11:55:24 +0530

    button added to check the user logged in or not

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+const RootPage = () => {
+    return (
+        <div>
+            RootPage (Protected)
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    );
+};
+export default RootPage;
