62b900dfa02ba444ff7c1f2f36f8603036bcc6e8
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-08-18 11:38:52 +0530

    chore : attached mobile side bar to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,6 +7,7 @@ import { Poppins, Comfortaa } from "next/font/google"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from "@/components/ModeToggle"
+import { MobileSidebar } from "@/components/MobileSidebar"
 
 
 const font = Comfortaa({
@@ -16,9 +17,9 @@ const font = Comfortaa({
 
 export const Navbar = () => {
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
             <div className="flex items-center">
-                <Menu className="md:hidden block" />
+                <MobileSidebar />
                 <Link href='/'>
                     <h1 className={cn("hidden md:block text-xl md:text-2xl font-extrabold text-primary", font.className)}>Companion-ai</h1>
                 </Link>
