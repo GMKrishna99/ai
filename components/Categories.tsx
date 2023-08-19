ef9793b5febb136e43cbbc8d70cd10e66cac4f18
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-08-19 09:28:45 +0530

    refactor : name changed

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -31,7 +31,7 @@ export const Categories = ({
             <button onClick={() => onClick(undefined)} className={cn(`flex items-center text-center text-xs md:text-xs px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition `,
                 !categoryId ? "bg-primary/25" : "bg-primary/10"
             )}>
-                Newest !
+                Newest
             </button>
             {data.map((item) => (
                 <button onClick={() => onClick(item.id)} key={item.id} className={cn(`flex items-center text-center text-xs md:text-xs px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition `,
